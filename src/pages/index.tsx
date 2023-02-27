b1d9c1be9c746255377e3554cc3516faa1941ba6
swikars1 <swikarsharma@gmail.com>
2023-02-27 18:45:49 +0545

    add dummy components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,28 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg w-max p-4">
+      {children}
+    </div>
+  );
+}
+
+function BeerInfo({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg w-max p-4">
+      {children}
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="">
       <h1 className="text-3xl font-bold">Hello </h1>
       <p className="text-3xl ">world </p>
+      <Card>aasd</Card>
     </div>
   );
 }
