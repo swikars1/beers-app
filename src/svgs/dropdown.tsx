980ecc528f82d42eb742e1faf0291f0c8c374c1e
swikars1 <swikarsharma@gmail.com>
2023-03-01 05:01:29 +0545

    dropdown icon

---
+++ src/svgs/dropdown.tsx
@@ -0,0 +1,17 @@
+export function DropdownIcon() {
+  return (
+    <svg
+      className="h-5 w-5"
+      fill="none"
+      viewBox="0 0 20 20"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={3}
+        d="M19 9l-7 7-7-7"
+      />
+    </svg>
+  );
+}
