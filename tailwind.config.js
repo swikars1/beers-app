d8709c1a1d003d95e200d71891d972df7a093d8b
swikars1 <swikarsharma@gmail.com>
2023-03-01 04:59:51 +0545

    packages setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("prettier-plugin-tailwindcss"),
+    require("daisyui"),
+  ],
 };
