b1513408df7f8a68f3fb4dd0871254ec27db0f4b
swikars1 <swikarsharma@gmail.com>
2023-03-01 17:29:05 +0545

    add loading

--- src/components/AllBeers.tsx
+++ src/components/AllBeers.tsx
@@ -15,7 +15,9 @@ export function AllBeers() {
   return (
     <div>
       {isLoading ? (
-        <p className="m-auto my-4 flex justify-center">Loading...</p>
+        <p className="m-auto my-4 mt-[300px] flex justify-center text-xl">
+          Loading...
+        </p>
       ) : (
         <BeerListWrapper>
           {beers?.pages?.map((page) =>
@@ -40,10 +42,12 @@ export function AllBeers() {
             }
           }}
         >
-          <div className="mb-6 mt-2 flex text-blue-500 hover:cursor-pointer">
-            <span className="mr-1 font-semibold">Load More</span>
-            <DropdownIcon />
-          </div>
+          {!isLoading && (
+            <div className="mb-6 mt-2 flex text-blue-500 hover:cursor-pointer">
+              <span className="mr-1 font-semibold">Load More</span>
+              <DropdownIcon />
+            </div>
+          )}
         </button>
       ) : null}
     </div>
