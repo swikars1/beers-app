c843fb04c90ba2832727e5e1dd6dfd075e67d131
swikars1 <swikarsharma@gmail.com>
2023-03-01 17:11:07 +0545

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,23 @@ import { MyBeers } from "@/components/MyBeers";
 import { TabSwitcher } from "@/components/TabSwitcher";
 import { appStore } from "@/store/beer.store";
 
+const TABS = [
+  {
+    id: 1,
+    name: "All Beers",
+    component: <AllBeers />,
+  },
+  {
+    id: 2,
+    name: "My Beers",
+    component: <MyBeers />,
+  },
+];
 export default function Home() {
-  const tabs = [
-    {
-      id: 1,
-      name: "All Beers",
-      component: <AllBeers />,
-    },
-    {
-      id: 2,
-      name: "My Beers",
-      component: <MyBeers />,
-    },
-  ];
-
   return (
     <div>
       <TabSwitcher
-        tabs={tabs}
+        tabs={TABS}
         headerRightComponent={
           <button
             className="btn-primary btn mr-4 capitalize lg:sm:mr-10"
